fae99e16de153fdf16c916c57962517c9e153915
James O'Hanlon <you@example.com>
2018-11-24 19:10:32 +0000

    second commit

---
+++ Assets/Scripts/DuckController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DuckController : MonoBehaviour
+{
+    public int points;
+    public float speed;
+    
+
+    private Transform playerPos;
+    private GrannyController player;
+    
+
+    void Start()
+    {
+        //Find Player
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<GrannyController>();
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        
+
+
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            //Player losing health
+        {
+            player.health--;
+
+
+           //Removes duck if hit
+            Destroy(gameObject);
+        }
+
+        if (other.CompareTag("Bread"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/GrannyController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GrannyController : MonoBehaviour
+{
+    
+    public float speed;
+    public int health = 10;
+    public Text healthDisplay;
+    
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    public int score;
+    public GameObject gameOverPanel;
+    // Use this for initialization
+    void Start()
+    {
+        score = 0;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthDisplay.text = "Lives :" + health;
+
+        if (health <= 0)
+        {
+           // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            GameOver();
+        }
+
+        {
+            Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            moveVelocity = moveInput.normalized * speed;
+        }
+    }
+        void FixedUpdate()
+
+        {
+            rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        }
+
+    void GameOver()
+    {
+        CancelInvoke();
+        gameOverPanel.SetActive(true);
+    }
+    
+
+
+
+
+}
